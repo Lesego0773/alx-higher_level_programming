a083f2bbf433445c488b8950378f3d1db593e141
Lesego0773 <kabelomakokga@gmail.com>
2024-01-23 06:36:50 -0800

    5-raise_exception.py

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 
 def raise_exception():
-    try:
-        # Attempt to perform an operation that raises a type exception
-        result = "string" + 42  # This will raise a TypeError
-    except TypeError as e:
-        print(f"Exception raised: {e}")
-
-# Example usage:
-raise_exception()
+    """Raise a TypeError exception."""
+    raise TypeError
 
