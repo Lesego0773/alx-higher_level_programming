8e67cf1a2a0fc8ca160af0bc33354d76d9108c80
Lesego0773 <kabelomakokga@gmail.com>
2024-04-30 20:56:56 -0700

    jj

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL
-- sends a POST request to the passed URL
-- takes email as a parameter
-- displays the body of the response
 """
-import sys
-import urllib.parse
-import urllib.request
+Script that takes in a URL and an email address, sends a POST request to the
+passed URL with the email as a parameter, and finally displays the body
+of the response.
+
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
+import requests
+from sys import argv
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    url = argv[1]
+    value = {"email": argv[2]}
+    req = requests.post(url, data=value)
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    print(req.text)
